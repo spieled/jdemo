0cd9750ef0feef3f6879168637344abc739e9ad8
liushaoping <shaoping.liu@56qq.cn>
2016-03-21 18:06:02 +0800

    add springmvc

---
+++ src/main/java/com/studease/jdemo/http/HelloController.java
@@ -0,0 +1,17 @@
+package com.studease.jdemo.http;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.RestController;
+
+/**
+ * Author: liushaoping
+ * Date: 2016/3/21.
+ */
+@RestController
+public class HelloController {
+    @RequestMapping("/hello.do")
+    public @ResponseBody String hello() {
+        return "hello world !!!!";
+    }
+}
